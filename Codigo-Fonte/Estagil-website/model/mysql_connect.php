2e45e7282b87b74212c729d68aa03d99c4c6fd81
Marcos <marcos.v.silva@live.com>
2016-12-16 18:15:15 -0200

    alteracao na conexao

--- Codigo-Fonte/Estagil-website/model/mysql_connect.php
+++ Codigo-Fonte/Estagil-website/model/mysql_connect.php
@@ -7,8 +7,8 @@ $dbname = "Estagil";
 
 //connection to the database
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=Estagil", $username, $password);
-    //$conn = new PDO("mysql:host=localhost>:3306;dbname=Estagil", $username, $password);
+    //$conn = new PDO("mysql:host=localhost;dbname=Estagil", $username, $password);
+    $conn = new PDO("mysql:host=localhost>:3306;dbname=Estagil", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 catch(PDOException $e)
